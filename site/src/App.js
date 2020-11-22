c3076a4a620ed28e51dc08a9dbf69d986423d25c
angelguajardo <41927488+angelguajardo@users.noreply.github.com>
2020-11-22 05:47:33 -0600

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -1,18 +1,64 @@
-import './App.css';
-import Carousel from './Carousel';
-import vid from './video.mp4'
+import React from 'react';
+import styles from './App.module.css';
+import {Line, Bar} from 'react-chartjs-2';
+import axios from 'axios';
+class App extends React.Component {
+  state = {
+    Names: [],
+    Counter: [],
+  }
+  
+  async componentDidMount(){
 
-function App() {
+    const axios = require('axios');
 
-  return (
-    <div className="App">
-      <Carousel />
-      <video autoPlay muted loop id="myVideo"
-        style={{position:'fixed', right:0, bottom:0, minWidth:'100%', minHeight:'100%', zIndex:-1}}>
-        <source src={vid}
-      type='video/mp4'/></video>
-    </div>
-  );
+    axios.get('https://us-central1-codechella-b0537.cloudfunctions.net/api/PlantNames').then(resp => {
+      console.log(resp.data);
+       this.setState({Names: resp.data});
+    });
+  }
+  
+
+  render(){
+    
+
+    const {Names} = this.state;
+    return(
+      
+      this.state.Names.map((skill) => {
+        return (
+          
+          <Bar
+            data = {{
+                labels: [skill.name],
+                datasets: [{
+                    lable: 'Whatever',
+                    backgroundColor:[
+                        'rgba(0,0,255,0.5)',
+                        'rgba(0,255,0,0.5)',
+                        'rgba(255,0,0,0.5)',
+                        'rgba(0,0,255,0.5)',
+                        'rgba(0,255,0,0.5)',
+                        'rgba(255,0,0,0.5)',
+                        'rgba(0,0,255,0.5)',
+                    ],
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data:[ skill.counter],
+                    
+                }]
+            }}
+          
+            />
+        );
+      })
+
+          
+     
+    )
+  }
 }
 
 export default App;
