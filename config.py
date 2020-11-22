e23333c84adcaeefebdc3364f63e064040eaa64b
crisg123 <43229662+crisg123@users.noreply.github.com>
2020-11-22 06:21:40 -0600

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-api_key = "sZ6tAowV11qMoX09iPg0wIaOG"
-api_secret = "ojppuK9SjKpO4zNALC0i3WgZJZjauiDL6z3l83NthRbM2WovIf"
-access_token = "1888484838-eMcre7hbsacQWNF2CAadXsr1TVUDV6aOEQvY5Cs"
-token_secret = "5R5i4hLTgDPeoIvAk2AJ5061aVM7Fp0g7UfwpwiBLPzZl"
-
-
-plant_api_key = "PT2LRnZuDENCCzD6Vl8u3V4FboW01gk9XAzuNpC7WoIPmybWe6"
